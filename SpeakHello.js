d280cd9d476512bb5f241f58674396de4e14f178
shivangi <uic.19mca8070@gmail.com>
2020-08-09 22:37:01 +0530

    first commit

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Kiran", "Shivangi", "Sapna", "Samsoor", "Riya", "Kawal", "Alisha", "Shweta", "Divya", "Shemona"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
